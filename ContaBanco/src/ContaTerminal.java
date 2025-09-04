b4f841722c016b6194c4fecbdc9b4dd7444945b8
Leandro Anjos <leandromanjos@gmail.com>
2025-09-04 20:27:17 -0300

    desafio completo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,39 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Dados de entrada
+        int numero;
+        double saldo;
+        String agencia;
+        String nomeCliente;
+        
+        //import class scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite a sua Agencia: ");
+        agencia = scanner.next();
+
+        System.out.println("Digite o número da Conta: ");
+        numero = scanner.nextInt();
+
+        System.out.println("Digite o seu Nome: ");
+        nomeCliente = scanner.next();
+
+        System.out.println("Digite o saldo com 'virgula nas casas decimais': ");
+        saldo = scanner.nextDouble();
+
+         // Formata como moeda brasileira
+        NumberFormat formatoMoeda = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        String valorFormatado = formatoMoeda.format(saldo);
+
+
+        //exibição dos dados
+        System.out.println("Olá "+ nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+
+        agencia+" , conta" +
+        numero+" e seu saldo "+
+        valorFormatado+" já está disponível para saque");
     }
 }
